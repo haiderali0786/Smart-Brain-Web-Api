3bf82514ca35ae60b7d67c2ab8d8f05818b23557
HAIDER ALI <haiderali0786@users.noreply.github.com>
2020-08-15 00:11:30 +0530

    Initlizing smart brain api files

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-     connectionString: process.env.DATABASE_URL,
-     ssl:true,
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'Haider@786',
+    database : 'smart_brain'
   }
 });
 
@@ -22,13 +24,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3001, ()=> {
-  console.log('app is running on port ${process.env.PORT}');
+app.listen(3001, ()=> {
+  console.log('app is running on port 3001');
 })
